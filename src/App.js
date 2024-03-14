5df82d5631b98ce0337310156618393945301929
jongo79 <jongfad@gmail.com>
2024-03-14 09:43:46 +0100

    Interfaz Simón

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [colors, setColor] = useState(['#030AFA', '#FAF303' , '#0AFA03', '#FA0E03'])
+
   return (
+    
     <div className="App">
+
       <header className="App-header">     
           Simon
       </header>
+
+      <div className="container">
+      {
+        colors.map((color, index) => {
+          return <div key={index} className={`pad pad-${index}`} style={{backgroundColor:`${color}`}}> </div>
+        })
+      }
+
+
     </div>
-  );
+    </div>
+
+  )
 }
 
 export default App;
+
+/*
+1. Interfaz de cartón
+2. Detectar click en los componentes
+3. Crear array para almacenar la secuencia del juego
+4. Crear el color random para añadirlo a la secuencia
+5. Reproducir la secuencia --HARD
+6. Usuario intenta reproducir secuencia
+7. Validar secuencia usuario por cada pulsación 
+8. Si acierta l apulsación sigue el juego
+9. Si no acierta el juego termina 
+
+
+
+
+
+*/
